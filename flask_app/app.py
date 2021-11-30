e253b53d1abac0e8b32765f43896051693e2c8ce
Ryan <ryan.stewart113@gmail.com>
2021-11-30 10:56:47 -0500

    add ssl configuration files

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello, Docker!'
+    headers = str(request.headers)
+    return f'''<h1>Hello, from the Flask App in Docker!</h1>\n
+    <p>{headers}</p>'''
